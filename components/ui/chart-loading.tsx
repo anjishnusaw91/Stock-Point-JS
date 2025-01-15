58b7c3a786ec14d82ca0a84625c87f55febcf451
Anjishnu Saw <sawanjishnu6@gmail.com>
2025-01-15 15:46:48 +0530

    update

---
+++ components/ui/chart-loading.tsx
@@ -0,0 +1,5 @@
+export const ChartLoading = () => (
+  <div className="flex items-center justify-center h-full min-h-[200px]">
+    <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+  </div>
+); 
