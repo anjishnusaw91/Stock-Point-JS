d9626f6db7583f918628e5dda2fa23211d88229d
Anjishnu Saw <99587721+anjishnusaw91@users.noreply.github.com>
2025-01-16 23:04:52 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,9 +6,3 @@ export default function LoginPage() {
 } 
 
 // stockpoint2025
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0aXRjdWtlZGN2aW5ycWJmYXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwMTE3ODAsImV4cCI6MjA1MjU4Nzc4MH0.0tk_U3zm9qWepeIf7kLoIOdR1o5YBbIajWzboRdSHxU
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0aXRjdWtlZGN2aW5ycWJmYXBiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNzAxMTc4MCwiZXhwIjoyMDUyNTg3NzgwfQ.pCSmfVAnX6S0h8iBMjxnHECMSK1-2pFO392JpKqGXac
-
-//
